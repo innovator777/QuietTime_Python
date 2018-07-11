f14403b8359c4b7d899b5554cdbd8e57a08502e2
innovator_mac <innovator777@naver.com>
2018-07-11 18:08:39 +0900

    answer.py => 서비스 사용 신청, 신청한 유저의 승인 및 해제, 예외상황 응답작업

--- functions/chatbot/answer.py
+++ functions/chatbot/answer.py
@@ -43,6 +43,50 @@ def getDBQTChoice():
     },
     'keyboard': {
       'type': 'buttons',
+      'buttons': [kw.getDBQTMainTitle(), kw.getHome()]
+    }
+  }
+
+def getUnappliedUserCase():
+  return {
+    'message': {
+      'text': '서비스 사용 신청이 되지 않은 분이시군요, 서비스를 이용하기 위해서는 서비스 사용 신청을 진행해 주시기 바랍니다.'
+    },
+    'keyboard': {
+      'type': 'buttons',
+      'buttons': [kw.getApply(), kw.getHome()]
+    }
+  }
+def getApplyManual():
+  return {
+    'message': {
+      'text': '이름/년생/성별을 입력해주세요. \n' +
+              '예시 => 홍길동/99년생/남성 \n\n'
+    },
+    'keyboard': {
+      'type': 'text'
+    }
+  }
+
+def getApplyQuietTime():
+  return {
+    'message': {
+      'text': '감사합니다. 관리자의 확인 작업 이후부터 서비스 사용이 가능해집니다.'
+    },
+    'keyboard': {
+      'type': 'buttons',
+      'buttons': [kw.getHome()]
+    }
+  }
+
+def getDisapprovedUserCase():
+  return {
+    'message': {
+      'text': '관리자의 확인이 되지 않은 상태입니다.\n' +
+              '관리자의 확인 작업 이후부터 서비스 사용이 가능해집니다.'
+    },
+    'keyboard': {
+      'type': 'buttons',
       'buttons': [kw.getHome()]
     }
   }
@@ -59,12 +103,130 @@ def getLLQT(text):
   }
 
 def getDBQT(text):
+  if getDataIsNoneDistinction(text):
+    return {
+      'message': {
+        'text': '오늘의 큐티가 없습니다.\n' +
+                '매일 12시 2분 이후부터 이용 가능합니다.'
+      },
+      'keyboard': {
+        'type': 'buttons',
+        'buttons': [kw.getDBQTMainTitle(), kw.getHome()]
+      }
+    }
+  else :
+    return {
+      'message': {
+        'text': text
+      },
+      'keyboard': {
+        'type': 'buttons',
+        'buttons': [kw.getDBQTMainTitle(), kw.getHome()]
+      }
+    }
+
+def getAdmin():
   return {
     'message': {
-      'text': text
+      'text': u'관리자로 확인되었습니다.'
     },
     'keyboard': {
       'type': 'buttons',
-      'buttons': [kw.getLLQTMainTitle(), kw.getDBQTMainTitle(), kw.getHome()]
+      'buttons': [kw.getDBQTMainTitle(), kw.getAdminFunction(), kw.getHome()]
     }
   }
+
+def getAdminMode():
+  return {
+    'message': {
+      'text': u'관리자 모드입니다.'
+    },
+    'keyboard': {
+      'type': 'buttons',
+      'buttons': [kw.getWaitList(), kw.getChangeUserState(), kw.getApproveList(), kw.getHome()]
+    }
+  }
+
+
+def getUserStateChangeMode():
+  return {
+    'message': {
+      'text' : u'유저키를 입력해주세요.'
+    },
+    'keyboard': {
+      'type': 'text'
+    }
+  }
+
+def getEnterIncorrectlyWord():
+  return {
+    'message': {
+      'text' : u'잘못 입력하셨습니다.'
+    },
+    'keyboard': {
+      'type': 'buttons',
+      'buttons': [kw.getWaitList(), kw.getChangeUserState(), kw.getApproveList(), kw.getHome()]
+    }
+  }
+
+def getWrongAccess():
+  return {
+    'message': {
+      'text' : u'잘못된 접근입니다. \n' +
+                '처음으로 돌아갑니다.'
+    },
+    'keyboard': {
+      'type': 'buttons',
+      'buttons': [kw.getTodaysQTMainTitle()]
+    }
+  }
+
+def getWaitUserList(text):
+  if getDataIsNoneDistinction(text):
+    return {
+      'message': {
+        'text': u'대기 유저가 없습니다.'
+      },
+      'keyboard': {
+        'type': 'buttons',
+        'buttons': [kw.getWaitList(), kw.getChangeUserState(), kw.getApproveList(), kw.getHome()]
+      }
+    }
+  else:
+    return {
+      'message': {
+        'text': text
+      },
+      'keyboard': {
+        'type': 'buttons',
+        'buttons': [kw.getWaitList(), kw.getChangeUserState(), kw.getApproveList(), kw.getHome()]
+      }
+    }
+
+def getApproveUserList(text):
+  if getDataIsNoneDistinction(text):
+    return {
+      'message': {
+        'text': u'사용중인 유저가 없습니다.'
+      },
+      'keyboard': {
+        'type': 'buttons',
+        'buttons': [kw.getWaitList(), kw.getChangeUserState(), kw.getApproveList(), kw.getHome()]
+      }
+    }
+  else:
+    return {
+      'message': {
+        'text': text
+      },
+      'keyboard': {
+        'type': 'buttons',
+        'buttons': [kw.getWaitList(), kw.getChangeUserState(), kw.getApproveList(), kw.getHome()]
+      }
+    }
+
+def getDataIsNoneDistinction(data):
+  if data is None:
+    return True
+  else:
+    return False
