d76d470ee7f7775894fc649e6e940e54f7bfc77e
innovator_mac <innovator777@naver.com>
2018-07-25 22:17:21 +0900

    Daily_Bible  순 타입 crawling 추가 작업

--- functions/crawler/daily_bible_crawler.py
+++ functions/crawler/daily_bible_crawler.py
@@ -4,13 +4,24 @@ from time import sleep
 
 import slack
 import dynamodb
-# import const
 import daily_bible_extract
 import env
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
 def handle(response, context):
+  todaysQuietTime = getTodaysQuiteTime(getSoup(env.getDailyBiblePath()))
+  todaysQuietTimeSoon = getTodaysQuiteTime(getSoup(env.getDailyBibleSoonPath()))
+
+  daily_bible_qt_table = dynamodb.get_daily_bible_qt_table()
+  dynamodb.insert_daily_bible_qt(daily_bible_qt_table, todaysQuietTime, todaysQuietTimeSoon)
+
+  slack.send_message(env.getChannelId(), '%s' % (todaysQuietTime))
+  slack.send_message(env.getChannelId(), '%s' % (todaysQuietTimeSoon))
+
+  return { 'status': 200 }
+
+def getSoup(path):
   options = webdriver.ChromeOptions()
   options.add_argument("--headless")
   options.add_argument("--disable-gpu")
@@ -29,7 +40,7 @@ def handle(response, context):
 
   driver = webdriver.Chrome("./bin/chromedriver", chrome_options=options)
   driver.implicitly_wait(3)
-  driver.get(env.getDailyBiblePath())
+  driver.get(path)
   sleep(3)
 
   html = driver.page_source
@@ -37,14 +48,7 @@ def handle(response, context):
 
   driver.close()
 
-  todaysQuietTime = getTodaysQuiteTime(soup)
-
-  daily_bible_qt_table = dynamodb.get_daily_bible_qt_table()
-  dynamodb.insert_daily_bible_qt(daily_bible_qt_table, todaysQuietTime)
-
-  slack.send_message(env.getChannelId(), '%s' % (todaysQuietTime))
-
-  return { 'status': 200 }
+  return soup
 
 def getTodaysQuiteTime(soup):
   mainTitle = daily_bible_extract.getMainTitle(soup)
